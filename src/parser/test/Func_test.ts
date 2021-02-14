d68512e74aa7564ac74336643fb3b78fcef6dc94
windy <kugoo11v12@126.com>
2021-02-15 01:27:15 +0800

    modify: 完善函数测试用例

--- src/parser/test/Func_test.ts
+++ src/parser/test/Func_test.ts
@@ -38,4 +38,17 @@ namespace tseval {
 		assert(result.default == valueB + valueA, "unmatch result")
 	})
 
+	autotest.addFunc("test call function 3", () => {
+		let valueA = 23
+		let valueB = 554
+		let valueC = 44
+
+		let tseval = new TSEval()
+		let statement = `export let default=${valueB}+aaa()()+${valueC}`
+		let result = tseval.execute<{ default: number }>(statement, {
+			aaa: function () { return function () { return valueA } },
+		})
+		assert(result.default == valueB + valueA + valueC, "unmatch result")
+	})
+
 }
